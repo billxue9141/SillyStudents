fa791cf14be9aaaf1cc9a8e64cde2ee13b7cb6ad
billxue9141 <billxue9141@hotmail.com>
2017-09-10 13:12:35 -0700

    added some indentation so it's more readable

--- Student.php
+++ Student.php
@@ -39,7 +39,7 @@ class Student {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' (' . $this->average() . ")\n";
         foreach ($this->emails as $which => $what)
-            $result .= $which . ': ' . $what . "\n";
+            $result .= '   ' . $which . ': ' . $what . "\n";
         $result .= "\n";
         return '<pre>' . $result . '</pre>';
     }
